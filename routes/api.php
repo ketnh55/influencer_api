cee31db065c8aae09aca731a2736808ec938e89d
ketnh55 <ket2.nguyen.huu@gmail.com>
2019-03-19 11:59:33 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
 });
 Route::prefix('v1')->group(function(){
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
     Route::post('/user_login_api', 'api\LoginAPIController@user_login_api')->name('login_api');
 });
